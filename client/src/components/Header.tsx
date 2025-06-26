f4c7327a9e2ab7f72374d5c4e0b8b43ef41f1d13
derivia <contato.lorransoares@gmail.com>
2025-06-26 18:59:17 -0300

    fix: fixing header layout on mobile

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
 	return (
 		<nav className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-md z-50 py-8">
-        <div className="container mx-auto px-10 flex justify-end items-center">
+        <div className="container mx-auto md:px-10 px-10 flex md:justify-end justify-center items-center">
 				<ul className="flex gap-12">
 					<li>
 						<a
